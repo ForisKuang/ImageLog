dc25163c13f46dccbef849d16d6b4aa76529c3f5
Yancong(Eddie) Huang <eddiehwangchinag@gmail.com>
2016-02-27 20:49:40 -0800

    check if grant access

--- access.py
+++ access.py
@@ -4,15 +4,10 @@ import ReadImage
 
 nubmer_of_tries = 3
 
-def main():
+def access():
 	condition = False
 	tries = 1
 	while not condtion or tries <= number_of_tries:
 		user_id = ReadImage.get_user_id()
 		json_data = ReadImage.get_json_data()
-		condition = Firebase.compareTo(json_data)
-		tries += 1
-	if condition:
-		Access(json_data)
-
-if __name__ == "__main__": main()
+	return condition
