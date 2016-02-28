33ba44a83dd7bff119a1090d7946bfa313718fff
ForisKuang <Forisk@uw.edu>
2016-02-27 17:14:44 -0800

    Label detection skeletal code call implemented

--- src/queries/GoogleVision.py
+++ src/queries/GoogleVision.py
@@ -1,29 +1,44 @@
+import argparse
+import base64
 import httplib2
-import sys
 
 from apiclient.discovery import build
-from oauth2client import tools
-from oauth2client.file import Storage
-from oauth2client.client import AccessTokenRefreshError
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import GoogleCredentials
 
-api_key = 'AIzaSyCk4z9TGxyQqz8g3Wy7G23NBem8B32N-J8'
+def main(photo_file):
+  '''Run a label request on a single image'''
 
-client_id = '1022914954389-5odc0vhkaglul6cglfs1c94loc777scf.apps.googleusercontent.com'
-client_key = 'nUpG1IVz6-bEQqCN2ymvpni5'
+  API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+  http = httplib2.Http()
 
-scope = 'https://console.google.com/apis/vision'
+  credentials = GoogleCredentials.get_application_default().create_scoped(
+      ['https://www.googleapis.com/auth/cloud-platform'])
+  credentials.authorize(http)
 
-flow = OAuth2WebServerFlow(client_id, client_key, scope)
+  service = build('vision', 'v1', http, discoveryServiceUrl=API_DISCOVERY_FILE)
 
-def main():
-	storage = Storage('credentials.dat')
-	credentials = storage.get()
-	if credentials is None or credentials.invalid:
-		credentials = tools.run_flow(flow, storage, tools.argparser.parse_args())
+  with open(photo_file, 'rb') as image:
+    image_content = base64.b64encode(image.read())
+    service_request = service.images().annotate(
+      body={
+        'requests': [{
+          'image': {
+            'content': image_content
+           },
+          'features': [{
+            'type': 'LABEL_DETECTION',
+            'maxResults': 1,
+           }]
+         }]
+      })
+    response = service_request.execute()
+    label = response['responses'][0]['labelAnnotations'][0]['description']
+    print('Found label: %s for %s' % (label, photo_file))
+    return 0
 
-	http = httplib2.Http()
-	http = credentials.authorize(http)
-
-	service = build('vision', 'v1.3', http=http)
-
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    'image_file', help='The image you\'d like to label.')
+  args = parser.parse_args()
+  main(args.image_file)
