1f07c4c2bedb8a16f15811afe45d6923f9f1080c
ForisKuang <Forisk@uw.edu>
2016-02-27 22:59:44 -0800

    I don't know what I'm doing and I'm sort of pissed

---
+++ javasrc/ReadImage.java
@@ -0,0 +1,13 @@
+public class ReadImage {
+
+	private String filename;
+
+	public ReadImage(filename) {
+		this.filename = filename;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+}
